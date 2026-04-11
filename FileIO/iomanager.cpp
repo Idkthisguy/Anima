8d64363755a83301eed38a3d3c1ece81c338bd28
Idkthisguy <idkf0ul@gmail.com>
2026-04-10 21:04:00 -0400

    heh

--- FileIO/iomanager.cpp
+++ FileIO/iomanager.cpp
@@ -1,9 +1,14 @@
+#include <QDebug>
+#include <QSettings>
 #include "iomanager.h"
 #include "anxhandler.h"
 #include "animafilehandler.h"
 #include "Export/mp4exporter.h"
 
-IOManager::IOManager(QObject* parent) : QObject(parent) {}
+IOManager::IOManager(QObject* parent) : QObject(parent) {
+    QSettings settings("AnimaStudio", "Anima");
+    m_recentFiles = settings.value("recentFiles").toStringList();
+}
 
 bool IOManager::saveProject(timelineManager* timeline) {
     if (m_path.isEmpty()) {
@@ -21,6 +26,23 @@ AnimaProject packProject(timelineManager* timeline) {
     return p;
 }
 
+void IOManager::addToRecents(const QString& path) {
+    qDebug() << "Architect: Registering project path to history:" << path;
+
+    m_recentFiles.removeAll(path);
+
+    m_recentFiles.prepend(path);
+
+    while (m_recentFiles.size() > 6) {
+        m_recentFiles.removeLast();
+    }
+
+    QSettings settings("AnimaStudio", "Anima");
+    settings.setValue("recentFiles", m_recentFiles);
+
+    emit recentFilesChanged();
+}
+
 bool IOManager::saveProjectAs(const QString& path, timelineManager* timeline) {
     AnimaProject proj = packProject(timeline);
     bool success = false;
@@ -33,13 +55,15 @@ bool IOManager::saveProjectAs(const QString& path, timelineManager* timeline) {
 
     if (success) {
         m_path = path;
+        addToRecents(path);
         emit currentPathChanged();
         markClean();
     }
     return success;
 }
 
-void IOManager::openProject(const QString& path, timelineManager* timeline) {
+
+bool IOManager::openProject(const QString& path, timelineManager* timeline) {
     AnimaProject proj;
 
     if (path.endsWith(".anx")) {
@@ -48,20 +72,26 @@ void IOManager::openProject(const QString& path, timelineManager* timeline) {
         proj = AnimaFileHandler::load(path);
     }
 
+    if (proj.frames.isEmpty()) return false;
+
     timeline->clearAll();
     timeline->setFps(proj.fps);
 
-    for(const auto& img : proj.frames) {
-        timeline->addFrame();
-        timeline->currentImage() = img;
-        timeline->next();
+    for (int i = 0; i < proj.frames.size(); ++i) {
+        if (i > 0) {
+            timeline->addFrame();
+        }
+        timeline->goTo(i);
+        timeline->currentImage() = proj.frames[i];
     }
 
     timeline->goTo(0);
-
     m_path = path;
+    addToRecents(path);
     emit currentPathChanged();
     markClean();
+
+    return true;
 }
 
 bool IOManager::exportToMP4(const QString& path, timelineManager* timeline) {
--- FileIO/iomanager.h
+++ FileIO/iomanager.h
@@ -16,6 +16,7 @@ class IOManager : public QObject {
     Q_OBJECT
     Q_PROPERTY(QString currentPath READ currentPath NOTIFY currentPathChanged)
     Q_PROPERTY(bool isDirty READ isDirty NOTIFY isDirtyChanged)
+    Q_PROPERTY(QStringList recentFiles READ recentFiles NOTIFY recentFilesChanged)
 
 public:
     explicit IOManager(QObject* parent = nullptr);
@@ -23,23 +24,29 @@ public:
     QString currentPath() const { return m_path; }
     bool isDirty() const { return m_dirty; }
 
+    QStringList recentFiles() const { return m_recentFiles; }
+
     Q_INVOKABLE bool saveProject(timelineManager* timeline);
     Q_INVOKABLE bool saveProjectAs(const QString& path, timelineManager* timeline);
-    Q_INVOKABLE void openProject(const QString& path, timelineManager* timeline);
+    Q_INVOKABLE bool openProject(const QString& path, timelineManager* timeline);
 
     Q_INVOKABLE bool exportToMP4(const QString& path, timelineManager* timeline);
 
     void markDirty() { if(!m_dirty) { m_dirty = true; emit isDirtyChanged(); } }
     void markClean() { if(m_dirty) { m_dirty = false; emit isDirtyChanged(); } }
 
+    void addToRecents(const QString& path);
+
 signals:
     void currentPathChanged();
     void isDirtyChanged();
     void errorOccurred(QString msg);
+    void recentFilesChanged();
 
 private:
     QString m_path;
     bool m_dirty = false;
+    QStringList m_recentFiles;
 };
 
 #endif
